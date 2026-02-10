6c1b09cad3bc07ec1a8c47219c33adf571485815
gil-peled <93687623+gil-peled@users.noreply.github.com>
2026-02-10 15:09:14 -0800

    Add files via upload

--- loadNavbar.js
+++ loadNavbar.js
@@ -12,21 +12,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
     <div class="main-menu">
         <ul>
             <li><a href="index.html">Home</a></li>
-            <li><a href="prototype_2.html">Prototype 2</a></li>
-            <li class="dropdown">
-                <a href="javascript:void(0)" class="dropbtn">Other</a>
-                <div class="dropdown-content">
-                    <a href="fib4_calculator.html">FIB-4</a>
-                    <a href="meld_calculator.html">MELD</a>
-                </div>
-            </li>
-            <li class="dropdown">
-                <a href="javascript:void(0)" class="dropbtn">Methodology</a>
-                <div class="dropdown-content">
-                    <a href="methodology.html">Overview</a>
-                    <a href="technical_methodology.html">Technical</a>
-                </div>
-            </li>
+            <li><a href="risk_calculator.html">Risk calculator</a></li>
+            <li><a href="other_calculators.html">Links to other calculators</a></li>
+            <li><a href="methodology.html">Methodology</a></li>
             <li><a href="contact.html">Contact</a></li>
         </ul>
     </div>
